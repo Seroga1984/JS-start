ad41a93aa568ef9991148d60e31606e070a6e853
Seroga1984 <Serogavod1984@gmail.com>
2022-07-30 22:04:32 +0300

    Добавлен модуль 2 JS

---
+++ index2.js
@@ -0,0 +1,74 @@
+// task 1
+// function checkAge(age) {
+//   if (age >= 18) {
+//     return "You are an adult";
+//   }
+
+//   return "You are a minor";
+// }
+
+// checkAge(20);
+// checkAge(8);
+// checkAge(14);
+// checkAge(38);
+
+
+
+// task 2
+// function checkPassword(password) {
+//   const ADMIN_PASSWORD = "jqueryismyjam";
+  
+
+  
+
+//   if (password === ADMIN_PASSWORD) {
+//     return 'Welcome!';
+//   }
+
+//   return 'Access denied, wrong password!'
+  
+// }
+
+// checkPassword("mangohackzor");
+// checkPassword("polyhax");
+// checkPassword("jqueryismyjam");
+
+
+
+// task 3
+// function checkStorage(available, ordered) {
+ 
+  
+
+//   if (ordered === 0) {
+//     return "Your order is empty!";
+//     }
+//    if (ordered > available) {
+//     return"Your order is too large, not enough goods in stock!";
+//   }
+//     return "The order is accepted, our manager will contact you";
+  
+
+  
+  
+// }
+// checkStorage(100, 50);
+// checkStorage(100, 130);
+// checkStorage(70, 0);
+// checkStorage(200, 20);
+// checkStorage(200, 250);
+// checkStorage(150, 0);
+
+
+
+// task 4
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+
+// task 5
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+
+// const firstElement = fruits[0];
+// const secondElement = fruits[1];
+// const lastElement = fruits[3];
