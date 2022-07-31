dcf5abef3c0e0315dc4715bf04baaf06c7f4afa1
Seroga1984 <Serogavod1984@gmail.com>
2022-07-31 21:25:37 +0300

    task 11

--- index2.js
+++ index2.js
@@ -44,7 +44,7 @@
 //     return "Your order is empty!";
 //     }
 //    if (ordered > available) {
-//     return"Your order is too large, not enough goods in stock!";
+//     return "Your order is too large, not enough goods in stock!";
 //   }
 //     return "The order is accepted, our manager will contact you";
   
@@ -72,3 +72,95 @@
 // const firstElement = fruits[0];
 // const secondElement = fruits[1];
 // const lastElement = fruits[3];
+
+
+
+// task 6
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+
+// fruits[1] = "peach";
+// fruits[3] = "banana";
+
+
+// task 7
+// const fruits = ["apple", "peach", "pear", "banana"];
+
+
+// const fruitsArrayLength = fruits.length;
+
+// task 8
+// const fruits = ["apple", "peach", "pear", "banana"];
+
+
+// const lastElementIndex = fruits.length - 1;
+// const lastElement = fruits[lastElementIndex];
+
+
+// task 9
+
+// function getExtremeElements(array) {
+  
+
+
+//   const lastElementIndex = array.length - 1;
+//   const lastElement = array[lastElementIndex];
+//   const firstElement = array[0];
+  
+//   return [firstElement, lastElement];
+// }
+
+  
+
+// getExtremeElements([1, 2, 3, 4, 5]);
+// getExtremeElements(["apple", "peach", "pear", "banana"]);
+
+// 1 способ
+    // const newArray = [];
+    // newArray.push(array[0], array[array.length - 1]);
+    // return newArray;
+//  2способ
+    //  const newArray = [array[0], array[array.length - 1]];
+    // return newArray;
+// 3 способ
+        // return  newArray = [array[0], array[array.length - 1]];
+  // 4 способ
+    // return [array[0], array[array.length - 1]];
+// }
+
+
+
+// task 10
+
+
+// function splitMessage(message, delimeter) {
+//   let words;
+  
+//    words = message.split(delimeter);
+  
+//   return words;
+// }
+
+// splitMessage("Mango hurries to the train", " ");
+// splitMessage("Mango", "");
+// splitMessage("best_for_week", "_");
+
+
+
+function calculateEngravingPrice(message, pricePerWord) {
+   
+   const message = [message];
+   const totalWord = message.length;
+   return totalWord * pricePerWord;
+
+
+
+
+   
+}
+
+calculateEngravingPrice("JavaScript is in my blood", 10);
+console.log(calculateEngravingPrice("JavaScript is in my blood", 10))
+calculateEngravingPrice("JavaScript is in my blood", 20);
+calculateEngravingPrice("Web-development is creative work", 40);
+calculateEngravingPrice("Web-development is creative work", 20);
